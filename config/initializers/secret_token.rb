c04412805aaea1c08d50831c65b7824385f84a5b
jcase146 <jcase146@gmail.com>
2013-02-04 19:20:12 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pins::Application.config.secret_token = '32bb0a02cd09be39bf3248d1b683ce9b1ace8b7eebd7284d2f1f26b595dadb8817e76493cdba21fcc901836948b1d1cbece30a01f5b34e914d1ebc6fb43fefa5'
